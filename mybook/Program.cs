6f9a95edc0d681b1aafabfe49dcbc7d1d549a97b
ehsyb <ehsyb@naver.com>
2020-05-19 10:28:34 +0900

    초기 커밋.

---
+++ mybook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mybook
+{
+    class Program
+    {
+
+        class book(){
+        String name = "PHP 프로그래밍 입문";
+        String firstsent = "2013년 5월 20일";
+        String write = "황재호";
+        String pp = "김태헌";
+        String pt = "한빛아카데미(주)";
+        String lewrite = "김현용";
+        String idea = "김이화";
+        String cc = "김이화";
+        String design = "여동일";
+        }
+    static void Main(string[] args)
+        {
+            
+        }
+    }
+}
